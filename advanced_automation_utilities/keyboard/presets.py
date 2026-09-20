be82ddbe089abab6327ad10b5174499ff49c728a
GuidoGross <profesional.gig.2802@outlook.com>
2026-09-20 11:00:07 -0300

    Keyboard presets updated to include all physics parameters.

--- advanced_automation_utilities/keyboard/presets.py
+++ advanced_automation_utilities/keyboard/presets.py
@@ -6,23 +6,35 @@ KEYBOARD_HUMAN = KeyboardPhysics(
     press_duration = 0.05,
     press_duration_variation = 0.1,
     hotkey_delay = 0.05,
-    hotkey_delay_variation = 0.1
+    hotkey_delay_variation = 0.1,
+    typing_error_chance = 0.025,
+    typing_error_correction_delay = 0.25,
+    typing_error_correction_delay_variation = 0.5,
+    typing_error_delayed_realization_chance = 0.5
 )
 
-KEYBOARD_SLOW = KeyboardPhysics(
+KEYBOARD_SLOW_AND_PRECISE = KeyboardPhysics(
     press_delay = 0.25,
     press_delay_variation = 0.1,
     press_duration = 0.1,
     press_duration_variation = 0.1,
     hotkey_delay = 0.25,
-    hotkey_delay_variation = 0.1
+    hotkey_delay_variation = 0.1,
+    typing_error_chance = 0.1,
+    typing_error_correction_delay = 0.5,
+    typing_error_correction_delay_variation = 0.5,
+    typing_error_delayed_realization_chance = 0.1
 )
 
-KEYBOARD_FAST = KeyboardPhysics(
+KEYBOARD_FAST_AND_IMPRECISE = KeyboardPhysics(
     press_delay = 0.05,
     press_delay_variation = 0.1,
     press_duration = 0.025,
     press_duration_variation = 0.1,
     hotkey_delay = 0.025,
-    hotkey_delay_variation = 0.1
+    hotkey_delay_variation = 0.1,
+    typing_error_chance = 0.05,
+    typing_error_correction_delay = 0.1,
+    typing_error_correction_delay_variation = 0.5,
+    typing_error_delayed_realization_chance = 0.75
 )
